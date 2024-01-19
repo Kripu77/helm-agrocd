6af9ad9584519dc73149d53d2de82b761020245e
Kripu Khadka <khadkakripu4@gmail.com>
2024-01-19 11:06:37 +1100

    Revert to 5c38767

---
+++ app/src/app.ts
@@ -0,0 +1,22 @@
+import Express, { Request, Response } from "express";
+const app = Express();
+const PORT: number = 8000;
+
+//get data
+
+app.get("/station", (req: Request, res: Response) => {
+  return res.status(200).json({
+    sucess: true,
+    data: "Live Synced on Sun 10 Sep",
+  });
+});
+
+//catch all routes
+app.use("*", (req: Request, res: Response) => {
+  return res.status(400).send("Invalid routes, please try later");
+});
+
+//listen for incomming requests
+app.listen(PORT, () => {
+  console.log("Server running");
+});
