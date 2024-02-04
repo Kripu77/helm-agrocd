f462375cdf612e8100a483c7c7b3ec8bdf123ed9
Kripu Khadka <khadkakripu4@gmail.com>
2024-02-04 14:08:21 +1100

    redis sentinel setup

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,13 +1,40 @@
 import Express, { Request, Response } from "express";
+import { Redis } from "ioredis";
 const app = Express();
 const PORT: number = 8000;
 
+//sentinel Nodes
+const sentinelNodes = [
+  {
+    host: "test-redis-node-0.test-redis-headless.em-svc-dev.svc.cluster.local",
+    port: 26379,
+  },
+  {
+    host: "test-redis-node-1.test-redis-headless.em-svc-dev.svc.cluster.local",
+    port: 26379,
+  },
+];
+//redis client
+const redisClient = new Redis({
+  sentinels: sentinelNodes,
+  name: "mymaster",
+});
+
+setInterval(async () => {
+  await redisClient.set("timeStamp", new Date().toISOString());
+}, 5000);
+
+async function getTimeStampFromRedis() {
+  return await redisClient.get("timeStamp");
+}
 //get data
 
-app.get("/station", (req: Request, res: Response) => {
+app.get("/station", async (req: Request, res: Response) => {
+  const redisTimeStamp = await getTimeStampFromRedis();
+
   return res.status(200).json({
-    sucess: true,
-    data: "Live Synced on Sun 10 Sep",
+    success: true,
+    data: redisTimeStamp,
   });
 });
 
